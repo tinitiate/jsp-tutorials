d0a68699f20d6c10fa8c0ba5140178f9f776a2e3
Venkata Bhattaram <tinitiate@users.noreply.github.com>
2013-08-29 12:33:12 -0400

    Create TinitiateExpLangWithJava.java

---
+++ jsp-expression-language/java-source/TinitiateExpLangWithJava.java
@@ -0,0 +1,27 @@
+package tinitiatejsp;
+
+// * TINITIATE.COM
+// * @FILENAME   TinitiateExpLangWithJava.java
+// * @NOTE:  This is the class used by the JSPExpLangJavaClass.jsp
+// *
+public class TinitiateExpLangWithJava {
+
+    // The Variables of the Class TinitiateExpLangWithJava
+    public int tinitiateVar1;
+    public int tinitiateVar2;
+
+    // Get and Set methods
+    public int gettinitiateVar1() {
+        return tinitiateVar1;
+    }
+    public void settinitiateVar1(int tinitiateVar1) {
+        this.tinitiateVar1 = tinitiateVar1;
+    }
+    public int gettinitiateVar2() {
+        return tinitiateVar2;
+    }
+    public void settinitiateVar2(int tinitiateVar2) {
+        this.tinitiateVar2 = tinitiateVar2;
+    }
+
+}
